4d92573093f5df5b58f4804fcf0549b577ddc895
Yuri Monteiro <yuri.monteiro@liferay.com>
2025-08-06 15:08:21 -0300

    LPD-59710 buildService

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectEntryVersionStatusException.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+package com.liferay.object.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Marco Leo
+ */
+public class ObjectEntryVersionStatusException extends PortalException {
+
+	public ObjectEntryVersionStatusException() {
+	}
+
+	public ObjectEntryVersionStatusException(String msg) {
+		super(msg);
+	}
+
+	public ObjectEntryVersionStatusException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public ObjectEntryVersionStatusException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
