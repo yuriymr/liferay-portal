6dc27a1833cc7c64dbbe598b2b320097c816c1fc
Yuri Monteiro <yuri.monteiro@liferay.com>
2026-04-13 14:16:44 -0300

    LPD-70691 Add new PhoneNumber field type in DDM

---
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/phonenumber/PhoneNumberCountryUtil.java
@@ -0,0 +1,73 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.dynamic.data.mapping.form.field.type.internal.phonenumber;
+
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.service.CountryLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Marco Leo
+ */
+public class PhoneNumberCountryUtil {
+
+	public static List<Map<String, String>> getCountries(
+		CountryLocalService countryLocalService) {
+
+		List<Map<String, String>> countriesList = new ArrayList<>();
+
+		long companyId = 0;
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			companyId = serviceContext.getCompanyId();
+		}
+
+		if (companyId == 0) {
+			return countriesList;
+		}
+
+		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
+
+		if (locale == null) {
+			locale = LocaleUtil.getDefault();
+		}
+
+		for (Country country :
+				countryLocalService.getCompanyCountries(companyId, true)) {
+
+			String idd = country.getIdd();
+
+			if (Validator.isNull(idd)) {
+				continue;
+			}
+
+			countriesList.add(
+				HashMapBuilder.put(
+					"a2", country.getA2()
+				).put(
+					"idd", idd
+				).put(
+					"name", country.getTitle(LocaleUtil.toLanguageId(locale))
+				).build());
+		}
+
+		return countriesList;
+	}
+
+}
---
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/object/dynamic/data/mapping/form/field/type/internal/phonenumber/PhoneNumberDDMFormFieldTemplateContextContributor.java
@@ -0,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.dynamic.data.mapping.form.field.type.internal.phonenumber;
+
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
+import com.liferay.dynamic.data.mapping.util.DDMFormFieldTemplateContextContributorUtil;
+import com.liferay.object.dynamic.data.mapping.form.field.type.constants.ObjectDDMFormFieldTypeConstants;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.CountryLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	property = "ddm.form.field.type.name=" + ObjectDDMFormFieldTypeConstants.PHONE_NUMBER,
+	service = DDMFormFieldTemplateContextContributor.class
+)
+public class PhoneNumberDDMFormFieldTemplateContextContributor
+	implements DDMFormFieldTemplateContextContributor {
+
+	@Override
+	public Map<String, Object> getParameters(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		DDMForm ddmForm = ddmFormField.getDDMForm();
+		boolean localizedObjectField = GetterUtil.getBoolean(
+			ddmFormField.getProperty("localizedObjectField"));
+
+		return HashMapBuilder.<String, Object>put(
+			"countries",
+			PhoneNumberCountryUtil.getCountries(_countryLocalService)
+		).put(
+			"localizedObjectField", localizedObjectField
+		).put(
+			() -> localizedObjectField ? "value" : null,
+			() -> _getValueJSONObject(ddmFormFieldRenderingContext)
+		).putAll(
+			DDMFormFieldTemplateContextContributorUtil.
+				getLocalizationParameters(
+					ddmFormField, ddmForm.getDefaultLocale())
+		).build();
+	}
+
+	private Object _getValueJSONObject(
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		try {
+			return _jsonFactory.createJSONObject(
+				ddmFormFieldRenderingContext.getValue());
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(jsonException);
+			}
+		}
+
+		return _jsonFactory.createJSONObject();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PhoneNumberDDMFormFieldTemplateContextContributor.class);
+
+	@Reference
+	private CountryLocalService _countryLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}
