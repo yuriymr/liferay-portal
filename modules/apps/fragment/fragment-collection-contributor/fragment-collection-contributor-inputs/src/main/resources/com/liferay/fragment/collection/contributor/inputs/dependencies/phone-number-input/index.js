73b6ad2e60e81caca206a82d3b9cb2ad48669509
Yuri Monteiro <yuri.monteiro@liferay.com>
2026-04-24 09:04:44 -0300

    LPD-83570 Consume injected country list in phone number input fragment

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/phone-number-input/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/phone-number-input/index.js
@@ -9,58 +9,19 @@ const inputsContainer = document.getElementById(
 	`${fragmentElementId}-inputs-container`
 );
 
-const COUNTRIES = [
-	{a2: 'US', idd: '1', name: 'United States'},
-	{a2: 'GB', idd: '44', name: 'United Kingdom'},
-	{a2: 'DE', idd: '49', name: 'Germany'},
-	{a2: 'FR', idd: '33', name: 'France'},
-	{a2: 'IT', idd: '39', name: 'Italy'},
-	{a2: 'ES', idd: '34', name: 'Spain'},
-	{a2: 'PT', idd: '351', name: 'Portugal'},
-	{a2: 'BR', idd: '55', name: 'Brazil'},
-	{a2: 'JP', idd: '81', name: 'Japan'},
-	{a2: 'CN', idd: '86', name: 'China'},
-	{a2: 'IN', idd: '91', name: 'India'},
-	{a2: 'AU', idd: '61', name: 'Australia'},
-	{a2: 'CA', idd: '1', name: 'Canada'},
-	{a2: 'MX', idd: '52', name: 'Mexico'},
-	{a2: 'AR', idd: '54', name: 'Argentina'},
-	{a2: 'CL', idd: '56', name: 'Chile'},
-	{a2: 'CO', idd: '57', name: 'Colombia'},
-	{a2: 'KR', idd: '82', name: 'South Korea'},
-	{a2: 'NL', idd: '31', name: 'Netherlands'},
-	{a2: 'BE', idd: '32', name: 'Belgium'},
-	{a2: 'CH', idd: '41', name: 'Switzerland'},
-	{a2: 'AT', idd: '43', name: 'Austria'},
-	{a2: 'SE', idd: '46', name: 'Sweden'},
-	{a2: 'NO', idd: '47', name: 'Norway'},
-	{a2: 'DK', idd: '45', name: 'Denmark'},
-	{a2: 'FI', idd: '358', name: 'Finland'},
-	{a2: 'PL', idd: '48', name: 'Poland'},
-	{a2: 'IE', idd: '353', name: 'Ireland'},
-	{a2: 'NZ', idd: '64', name: 'New Zealand'},
-	{a2: 'SG', idd: '65', name: 'Singapore'},
-	{a2: 'ZA', idd: '27', name: 'South Africa'},
-	{a2: 'RU', idd: '7', name: 'Russia'},
-	{a2: 'TR', idd: '90', name: 'Turkey'},
-	{a2: 'IL', idd: '972', name: 'Israel'},
-	{a2: 'AE', idd: '971', name: 'United Arab Emirates'},
-	{a2: 'SA', idd: '966', name: 'Saudi Arabia'},
-	{a2: 'TH', idd: '66', name: 'Thailand'},
-	{a2: 'PH', idd: '63', name: 'Philippines'},
-	{a2: 'ID', idd: '62', name: 'Indonesia'},
-	{a2: 'MY', idd: '60', name: 'Malaysia'},
-];
+function getCountriesByIddLength() {
+	const countries = [];
 
-function getFlag(a2) {
-	return String.fromCodePoint(
-		...[...a2.toUpperCase()].map((c) => 0x1f1e6 + c.charCodeAt(0) - 65)
-	);
+	for (const option of countrySelect.options) {
+		const idd = option.getAttribute('data-idd');
+
+		if (idd) {
+			countries.push({a2: option.value, idd});
+		}
+	}
+
+	return countries.sort((a, b) => b.idd.length - a.idd.length);
 }
-
-const COUNTRIES_BY_IDD_LENGTH = [...COUNTRIES].sort(
-	(a, b) => b.idd.length - a.idd.length
-);
 
 function parsePhoneValue(value) {
 	if (!value || !value.startsWith('+')) {
@@ -69,7 +30,7 @@ function parsePhoneValue(value) {
 
 	const digits = value.slice(1);
 
-	for (const country of COUNTRIES_BY_IDD_LENGTH) {
+	for (const country of getCountriesByIddLength()) {
 		if (digits.startsWith(country.idd)) {
 			return {
 				countryA2: country.a2,
@@ -104,18 +65,6 @@ function main() {
 	if (!displayInput || !countrySelect) {
 		return;
 	}
-
-	COUNTRIES.sort((a, b) => a.name.localeCompare(b.name));
-
-	COUNTRIES.forEach((country) => {
-		const option = document.createElement('option');
-
-		option.value = country.a2;
-		option.setAttribute('data-idd', country.idd);
-		option.textContent =
-			getFlag(country.a2) + ' +' + country.idd + ' ' + country.name;
-		countrySelect.appendChild(option);
-	});
 
 	const currentValue = displayInput.value || '';
 
